ed0f6c070d28e3e7a0fb249fdbbafb1caf2731b2
Miquel Castro <castro-miquel@hotmail.com>
2021-05-17 11:48:12 +0200

    Creating ImageColorController with mostUsedColor

---
+++ app/Http/Controllers/ImageColorController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+use Validator;
+
+use Illuminate\Http\Request;
+
+class ImageColorController extends Controller
+{
+    public function mostUsedColor(Request $request){
+
+
+    }
+}
